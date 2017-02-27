b38cd59e0faa95b7e18c0ebfc19d8cd96c389cdf
Alex <Alex@DESKTOP-002G4ER>
2017-02-26 20:49:53 -0500

    se crea el proyecto2

--- src/cola/fifo/Cola.java
+++ src/cola/fifo/Cola.java
@@ -5,10 +5,46 @@
  */
 package cola.fifo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Alex
+ * Se basa en primero q entra primero que sale
+ *GUIA: https://www.youtube.com/watch?v=jUNcDIJ0S_k
+ * 
  */
 public class Cola {
+    
+    private ArrayList<Object> fifo= new ArrayList<>();
+
+    public Cola() {
+    }
+    
+    
+    
+    public void offer(){
+    fifo.add(0);
+    }
+    
+    public Object peek(){
+        if (!(fifo.isEmpty())) {
+            return fifo.get(0);
+        }else{
+            return null;
+        }
+    }
+    
+    public Object poli(){
+        
+        if (!(fifo.isEmpty())) {
+            Object o = fifo.get(0);
+            fifo.remove(0);
+            return o;
+        }else{
+            return null;
+        }
+    }
+    
     
 }
